9b310b962f7c2188e40fcb9f37f6d4a39e0adf6a
root <root@DESKTOP-VSGVRGR.localdomain>
2020-08-10 00:42:57 +0530

    commit

--- src/app.js
+++ src/app.js
@@ -20,42 +20,61 @@ const PLAYERS = [
     "Vader",
     "Slingo"
 ];
-
-// initialize players with image and strength
 const initPlayers = (players) => {
-    //let name,image,strength,type;
-    //let emp=[];
-    let detailedPlayers = {};
-    // Create players using for loop
-    // Type your code here
-    for(let i=0;i<players.length;i++){
-        if(i%2==0) type="hero";
-            else type="villian";
-        detailedPlayers={name:PLAYERS[i],image:"images/super-"+[i+1]+".png",strength:getRandomStrength(),
-        type:type[i]}; 
-    }
-            return detailedPlayers;
+// initialize players with image and strength
+//let emp = [];
+  let detailedPlayers = []; //{name,image,strength,type};
+  // Create players using for loop
+  // Type your code here
+  for (let i = 0; i < players.length; i++) {    
+    detailedPlayers.push({
+      name: players[i],
+      image: "images/super-" + [i + 1] + ".png",
+      strength: getRandomStrength(),
+      type: (i % 2 == 0)? "hero" : "villain"
+    });
+    console.log(detailedPlayers);
+  return detailedPlayers;
+}
 }
 
 // getting random strength
-const getRandomStrength = () =>{ Math.ceil(Math.random()*100);
-    // Return a random integer (0,100]
-    // Note: You can use Math.random() and Math.ceil()
+const getRandomStrength = () => {
+  return Math.ceil(Math.random() * 100);
+  // Return a random integer (0,100]
+  // Note: You can use Math.random() and Math.ceil()
+};
+function typ(t,pl){
+  
 }
-
 const buildPlayers = (players, type) => {
-    let fragment = '';
-
-    
-    for(let i=0;i<players.length;i++){
-        const p=document.createElement('div');
-    }
-    // Loop through players and accumulate HTML template
-    // depending of type of player(hero|villain)
-    // Type your code here
-
-    return fragment;
+//   let fragment = document.innerHTML = `
+//   ${players.filter((type,players)=> {
+//     if(players.type == type) {
+//       `<div class="player">
+//   <img src=${players.image}>
+//   <div class="name">${players.name}</div>
+//   <div class="strength">${players.strength}</div>
+// </div>`}})}`
+let fragment = document.createElement("div");
+fragment.classList.add('team');
+let player,img,name,strength;
+for (let i = 0; i < players.length; i++) {
+  if (players[i].type == type) {
+     img = document.createElement("img");
+    img.setAttribute("src", `${players[i].image}`);
+    name = document.createElement("div");
+    name.append(`${players[i].name}`);
+    strength = document.createElement("div");
+    strength.append(`${players[i].strength}`);
+    fragment.appendChild(img);
+    fragment.appendChild(name);
+    fragment.appendChild(strength);
+  }
 }
+fragment = fragment.innerHTML;
+return fragment;
+};
 // Display players in HTML
 
 const viewPlayers = (players) => {
